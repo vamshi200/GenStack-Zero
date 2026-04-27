ae376357c215d15033943fc06c5e0c16681855a6
Vamshi Kardhanoori <vammshi0204@gmail.com>
2026-04-27 12:28:05 -0500

    Remove portfolio app and restore GenStack Zero

--- vamshi-portfolio/src/data/site.js
+++
@@ -1,6 +0,0 @@
-export const siteConfig = {
-  name: 'Vamshi',
-  title: 'Vamshi Portfolio Setup Complete',
-  github: 'https://github.com/vamshi200',
-  linkedin: 'https://www.linkedin.com/in/vamshi-kardhanoori/',
-}
